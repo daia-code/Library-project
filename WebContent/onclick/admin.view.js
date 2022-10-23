021b8546e22ebae985011f115f4dd730faec09cf
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-10-23 21:00:51 +0300

    Update admin.view.js

--- WebContent/onclick/admin.view.js
+++ WebContent/onclick/admin.view.js
@@ -24,9 +24,16 @@ sap.ui.jsview("onclick.admin", {
 		}).addStyleClass("textWelcome");
 		var oLabelUser = new sap.m.Label("nameAdmin")
 				.addStyleClass("userWelcome");
+		var libraryLocal = new sap.m.Button({
+			icon : "sap-icon://functional-location",
+			press : [ oController.openInfoLibrary, oController ]
+		}).addStyleClass("libraryLocal");
+		var settingsBtn = new sap.m.Button({
+			icon : "sap-icon://account",
+			press : [ oController.openSettingsDialog, oController ]
+		}).addStyleClass("settingsBtn");
 		var logOut = new sap.m.Button({
 			icon : "sap-icon://action",
-
 			press : function() {
 				var adminName = sap.ui.getCore().byId("inputUser");
 				adminName.setValue("");
@@ -36,37 +43,76 @@ sap.ui.jsview("onclick.admin", {
 			}
 
 		}).addStyleClass("logOutBtn");
-		var readerList=  new sap.m.List({
-			headerText:"Readers:"
-	     });
-		
-		var readerInfo=new sap.m.StandardListItem({
-			title:"Information about reader"
-			
-		        	
+		var readerList = new sap.m.List({
+			headerText : "Readers:"
 		});
-		var readerBook=new sap.ui.table.Table({
-			title:"History reading"
-		        	
+
+		var readerInfo = new sap.m.StandardListItem({
+			title : "Information about reader"
+
+		});
+		var readerBook = new sap.ui.table.Table({
+			title : "History reading"
+
 		}).addStyleClass("historyR");
 		var readerBox = new sap.ui.layout.VerticalLayout({
 			content : [ readerInfo, readerBook ]
 		})
-		var spliter=new sap.ui.layout.Splitter({
-			contentAreas:[
-			   readerList, readerBox]          
+		var spliter = new sap.ui.layout.Splitter({
+			contentAreas : [ readerList, readerBox ]
+		});
+		var dialogSettings = new sap.m.Dialog("settingsDialog", {
+			title : "Settings account",
+			content : [ new sap.m.Button({
+				text : "Close",
+				type : sap.m.ButtonType.Emphasized,
+				press : function() {
+					dialogSettings.close();
+				}
+
+			}) ]
+		});
+		var dialogLibrary = new sap.m.Dialog("infoAdmin", {
+			title : "Info",
+			content : [ new sap.ui.layout.VerticalLayout({
+				content : [ new sap.m.Text({
+					text : "Library OnClick"
+				}).addStyleClass("infoName"),
+				new sap.m.Text({
+					text : "Address: University Street no.1"
+				}).addStyleClass("infoGeneral"),
+				new sap.m.Text({
+					text : "Schedule"
+				}).addStyleClass("infoSchel"),
+				new sap.m.Text({
+					text : "OPEN: Monday-Friday: 8 am - 8 pm"
+				}).addStyleClass("infoGeneral"), 
+				new sap.m.Text({
+					text : "OPEN: Saturday: 9 am - 1 pm"
+				}).addStyleClass("infoGeneral"), 
+				new sap.m.Text({
+					text : "CLOSE: Sunday"
+				}).addStyleClass("infoGeneral"),
+				new sap.m.Text({
+					text : "domain:http://localhost:50000/onclick"
+				}).addStyleClass("infoGeneral"),
+				new sap.m.Button({
+					text : "Close",
+					type:sap.m.ButtonType.Success,
+					press : function() {
+						dialogLibrary.close();
+					}
+				}).addStyleClass("infoBtn"), ]
+			}),
+
+			]
 		});
 		var oPage = new sap.m.Page({
 			title : "Controll page",
 			showNavButton : true,
-			navButtonPress : function() {
-				var adminName = sap.ui.getCore().byId("inputUser");
-				adminName.setValue("");
-				var pass = sap.ui.getCore().byId("passUser");
-				pass.setValue("");
-				app.back();
-			},
-			content : [ oLabel, oLabelUser, logOut, spliter ]
+			navButtonPress : [ oController.appBack, oController ],
+			content : [ oLabel, oLabelUser, libraryLocal, settingsBtn, logOut,
+					spliter ]
 		});
 		return oPage
 	}
